cb5349ee109bd1a5c12d017039e8c85f8ce7ad53
tankaixiong@aliyun.com <tankaixiong@aliyun.com>
2015-10-09 18:06:06 +0800

    ScanClass

--- src/main/java/tank/http/annotation/api/AnnotationParser.java
+++ src/main/java/tank/http/annotation/api/AnnotationParser.java
@@ -1,7 +1,11 @@
 package tank.http.annotation.api;
 
-import java.lang.reflect.Field;
-import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 /**
  * @author tank
@@ -10,40 +14,11 @@ import java.util.Vector;
  * @version :1.0
  */
 public class AnnotationParser {
-	
-	public static void init(String [] scanPackage){
-		
-		
+
+	public static void init(String[] scanPackage) {
+
 	}
 
 	public static void main(String[] args) {
-		
-		 System.out.println(HttpMethod.GET);
-		
-		try {
-			Field field = ClassLoader.class.getDeclaredField("classes");  
-			field.setAccessible(true);//设置该成员变量为可访问  
-			
-			Vector<Class<?>> classListClasses=(Vector<Class<?>>) field.get(ClassLoader.getSystemClassLoader());
-			
-			 for (Class<?> cz : classListClasses) {
-				System.out.println(cz.getName());
-			}
-			
-			
-		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-
 	}
 }
